769fbe26899994c4f50678c842766a6693438db5
raisgame-instructor <raisgame@outlook.com>
2024-01-28 13:38:07 -0500

    added sound support for rizz, edited trigger vol script
    added sound support for rizz, edited trigger vol script PICKUPS BROKEN

--- Assets/Scripts/RizzMeter.cs
+++ Assets/Scripts/RizzMeter.cs
@@ -12,7 +12,9 @@ public class RizzMeter : MonoBehaviour
     public Canvas GameOverCanvas;
     public MusicOmatic MusicAudioSource;
     public GameOverScript GameOverScript;
-
+    //public AudioSource soundplayer;
+    public AudioClip downrizz;
+    public AudioClip uprizz;
     /*on update we want the bar to go down by a very small amount and check to see if the value is zero
     When the bar gets to Zero we want to trigger the end of the game*/
     void Update()
@@ -40,10 +42,16 @@ public class RizzMeter : MonoBehaviour
     {
         CurrentValue = CurrentValue + 15f;
         Rizzmeter.value = CurrentValue;
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = uprizz;
+        audio.Play();
     }
     //at the end of every conversation we want the bar to go down by a drastic amount
     public void NegUpdateRizz()
     {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = downrizz;
+        audio.Play();
         CurrentValue = CurrentValue - 20f;
         Rizzmeter.value = CurrentValue;
     }
